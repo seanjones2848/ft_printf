c16539b3cac85f9a11e802b1b6ca6876e830e0cc
seanjones2848 <seanjones2848@gmail.com>
2017-11-28 12:51:55 -0800

    bloop

--- src/stringify.c
+++ src/stringify.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 13:01:44 by sjones            #+#    #+#             */
-/*   Updated: 2017/11/27 15:00:32 by sjones           ###   ########.fr       */
+/*   Updated: 2017/11/28 12:48:57 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ static void	print_spaces(t_print *p)
 void		stringify(t_print *p)
 {
 	p->arg.s = va_arg(p->args, char*);
-	if (!p->arg.s)
-		write(p->fd, "(null)", 6);
+	if (!p->arg.s && write(p->fd, "(null)", 6))
+		return;
 	p->len = ft_strlen(p->arg.s);
 	if (p->minus)
 	{
